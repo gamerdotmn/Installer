912fa7fbbd653e3db6afabbc613787081ef1384e
gamerdotmn <gamerdotmn@gmail.com>
2017-01-19 13:37:54 +0800

    2017.1.19

---
+++ Installer/Main.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Installer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label_choose = new System.Windows.Forms.Label();
+            this.radioButton_client = new System.Windows.Forms.RadioButton();
+            this.radioButton_monitor = new System.Windows.Forms.RadioButton();
+            this.radioButton_server = new System.Windows.Forms.RadioButton();
+            this.button_install = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label_installing = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(35, 129);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(411, 23);
+            this.progressBar.TabIndex = 0;
+            // 
+            // label_choose
+            // 
+            this.label_choose.AutoSize = true;
+            this.label_choose.Location = new System.Drawing.Point(30, 30);
+            this.label_choose.Name = "label_choose";
+            this.label_choose.Size = new System.Drawing.Size(51, 13);
+            this.label_choose.TabIndex = 1;
+            this.label_choose.Text = "Сонголт:";
+            // 
+            // radioButton_client
+            // 
+            this.radioButton_client.AutoSize = true;
+            this.radioButton_client.Checked = true;
+            this.radioButton_client.Location = new System.Drawing.Point(33, 60);
+            this.radioButton_client.Name = "radioButton_client";
+            this.radioButton_client.Size = new System.Drawing.Size(61, 17);
+            this.radioButton_client.TabIndex = 1;
+            this.radioButton_client.TabStop = true;
+            this.radioButton_client.Text = "Клиент";
+            this.radioButton_client.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_monitor
+            // 
+            this.radioButton_monitor.AutoSize = true;
+            this.radioButton_monitor.Location = new System.Drawing.Point(33, 83);
+            this.radioButton_monitor.Name = "radioButton_monitor";
+            this.radioButton_monitor.Size = new System.Drawing.Size(61, 17);
+            this.radioButton_monitor.TabIndex = 2;
+            this.radioButton_monitor.Text = "Хяналт";
+            this.radioButton_monitor.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_server
+            // 
+            this.radioButton_server.AutoSize = true;
+            this.radioButton_server.Location = new System.Drawing.Point(33, 106);
+            this.radioButton_server.Name = "radioButton_server";
+            this.radioButton_server.Size = new System.Drawing.Size(62, 17);
+            this.radioButton_server.TabIndex = 3;
+            this.radioButton_server.Text = "Сервер";
+            this.radioButton_server.UseVisualStyleBackColor = true;
+            // 
+            // button_install
+            // 
+            this.button_install.Location = new System.Drawing.Point(371, 158);
+            this.button_install.Name = "button_install";
+            this.button_install.Size = new System.Drawing.Size(75, 23);
+            this.button_install.TabIndex = 5;
+            this.button_install.Text = "Суулгах";
+            this.button_install.UseVisualStyleBackColor = true;
+            this.button_install.Click += new System.EventHandler(this.button_install_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = global::Installer.Properties.Resources.image_1007767;
+            this.pictureBox.Location = new System.Drawing.Point(192, 30);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox.TabIndex = 6;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Visible = false;
+            // 
+            // label_installing
+            // 
+            this.label_installing.AutoSize = true;
+            this.label_installing.Location = new System.Drawing.Point(152, 133);
+            this.label_installing.Name = "label_installing";
+            this.label_installing.Size = new System.Drawing.Size(177, 13);
+            this.label_installing.TabIndex = 7;
+            this.label_installing.Text = "Суулгаж байна, та түр хүлээнэ үү.";
+            this.label_installing.Visible = false;
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            this.backgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker_RunWorkerCompleted);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 211);
+            this.Controls.Add(this.label_installing);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.button_install);
+            this.Controls.Add(this.radioButton_server);
+            this.Controls.Add(this.radioButton_monitor);
+            this.Controls.Add(this.radioButton_client);
+            this.Controls.Add(this.label_choose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "MasterCafe суулгах";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label_choose;
+        private System.Windows.Forms.RadioButton radioButton_client;
+        private System.Windows.Forms.RadioButton radioButton_monitor;
+        private System.Windows.Forms.RadioButton radioButton_server;
+        private System.Windows.Forms.Button button_install;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label_installing;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+    }
+}
---
+++ Installer/Main.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Principal;
+using System.Net;
+using System.IO.Compression;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
+
+namespace Installer
+{
+    public partial class Main : Form
+    {
+        public static bool start = false;
+        public static string host = "gamer.mn";
+        System.Timers.Timer timer = new System.Timers.Timer(1000);
+        
+        public Main()
+        {
+            
+            bool osok = false;
+            if (System.Environment.OSVersion.Version.Major == 6 && System.Environment.OSVersion.Version.Minor == 1)
+            {
+                osok = true;
+            }
+            if (osok == false)
+            {
+                MessageBox.Show("Үйлдлийн систем дэмжихгүй. Зөвхөн Windows 7 зөвшөөрнө.", "Анхаар", MessageBoxButtons.OK);
+                System.Environment.Exit(0);
+            }
+            bool isElevated;
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            if (isElevated == false)
+            {
+                MessageBox.Show(this, "Үйлдлийн системийн хэрэглэгчийн эрх хүрэлцэхгүй байна. 'Run as Administrator' ашиглана уу.", "Mastercafe Installer");
+                System.Environment.Exit(0);
+            }
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+            InitializeComponent();
+        }
+
+        void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Process[] ps = Process.GetProcesses();
+            for (int i = 0; i < ps.Length; i++)
+            {
+                if (ps[i].ProcessName == "pcaui")
+                {
+                    ps[i].Kill();
+                }
+            }
+            /*
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(() => { Text = c+""; }));
+            else Text = c+"";
+            */
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressPercentage;
+        }
+        
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error == null)
+            {
+                label_installing.Visible = true;
+                pictureBox.Visible = true;
+                label_choose.Visible = false;
+                radioButton_client.Visible = false;
+                radioButton_monitor.Visible = false;
+                radioButton_server.Visible = false;
+                progressBar.Visible = false;
+                button_install.Visible = false;
+                backgroundWorker.RunWorkerAsync();
+            }
+            else
+            {
+                MessageBox.Show("Суулгац татахад алдаа гарлаа.","Анхаар",MessageBoxButtons.OK);
+                System.Environment.Exit(0);
+            }
+        }
+
+        private void button_install_Click(object sender, EventArgs e)
+        {
+            start = true;
+            radioButton_client.Enabled = false;
+            radioButton_monitor.Enabled = false;
+            radioButton_server.Enabled = false;
+            button_install.Enabled = false;
+
+            if (System.IO.Directory.Exists(@"C:\mc")==false)
+            {
+                System.IO.Directory.CreateDirectory(@"C:\mc");
+            }
+            if (System.IO.Directory.Exists(@"C:\mc\server") == false)
+            {
+                System.IO.Directory.CreateDirectory(@"C:\mc\server");
+            }
+            if (radioButton_server.Checked)
+            {
+                if (System.IO.File.Exists(@"C:\mc\server.tar.gz"))
+                {
+                    System.IO.File.Delete(@"C:\mc\server.tar.gz");
+                }
+                WebClient webClientVersion = new WebClient();
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri(@"http://"+host+@"/api/installs/server.tar.gz"), @"C:\mc\server\server.tar.gz");
+            }
+        }   
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (start)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (radioButton_server.Checked)
+            {
+                Stream inStream = File.OpenRead(@"C:\mc\server\server.tar.gz");
+                Stream gzipStream = new GZipInputStream(inStream);
+
+                TarArchive tarArchive = TarArchive.CreateInputTarArchive(gzipStream);
+                tarArchive.ExtractContents(@"C:\mc\server");
+                tarArchive.Close();
+
+                gzipStream.Close();
+                inStream.Close();
+            }
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            Text = "MasterCafe суулгах" + " ("+e.ProgressPercentage+"%)";
+        }
+
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (radioButton_server.Checked)
+            {
+                bool ge = true;
+                RegistryKey RKN = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\NET Framework Setup\NDP\v4.0");
+                if (RKN == null)
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = @"C:\mc\server\dotnet\setup.exe";
+                    process.StartInfo.Arguments = @" /q";
+                    process.StartInfo.ErrorDialog = true;
+                    process.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                    process.Start();
+                    process.WaitForExit();
+                }
+                //pcaui.exe
+                RegistryKey RKS = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Microsoft SQL Server\90");
+                if (RKS == null)
+                {
+                    
+                    Process process = new Process();
+                    process.StartInfo.FileName = @"C:\mc\server\sqlexpr\setup.exe";
+                    process.StartInfo.Arguments = @" /qn ADDLOCAL=ALL SECURITYMODE=SQL SAPWD=pldifvzz7x DISABLENETWORKPROTOCOLS=0 ENABLERANU=0 INSTANCENAME=mastercafe";
+                    process.StartInfo.ErrorDialog = true;
+                    process.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+                    process.Start();
+                    process.WaitForExit();
+
+                    SqlConnection con = new SqlConnection(@"Data Source=127.0.0.1\MASTERCAFE;Persist Security Info=True;User ID=sa;Password=pldifvzz7x;MultipleActiveResultSets=True;");
+                    con.Open();
+
+                    SqlCommand cmd = new SqlCommand(Installer.Properties.Resources.create, con);
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    cmd = null;
+
+                    cmd = new SqlCommand(Installer.Properties.Resources.dbcfg, con);
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    cmd = null;
+
+                    cmd = new SqlCommand(Installer.Properties.Resources.generate, con);
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    cmd = null;
+                     
+                    cmd = new SqlCommand(Installer.Properties.Resources.insert, con);
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    cmd = null;
+
+                    con.Close();
+                    con.Dispose();
+                    con = null;
+                }
+                
+                string deskdir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                if (File.Exists(deskdir + @"\server.lnk"))
+                {
+                    File.Delete(deskdir + @"\server.lnk");
+                }
+                File.Copy(@"C:\mc\server\server.lnk", deskdir + @"\server.lnk");
+                Process.Start(@"C:\mc\server\server.exe");
+                System.Environment.Exit(0);
+            }
+        }
+        
+    }
+}
